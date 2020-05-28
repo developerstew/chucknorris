044821ba281c7f4c1f3470f176ff3628b199cfd1
dewald greeff <dewald@developerstew.com>
2020-05-28 21:06:41 +0200

    changed heroku postbuild

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ app.use(
     graphiql: true,
   })
 );
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '/public', 'index.html'));
 });
-app.use(express.static('public'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
