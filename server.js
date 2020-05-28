1439d59132126398a305d0af6167d4c9929a8f26
dewald greeff <dewald@developerstew.com>
2020-05-28 09:22:46 +0200

    Added React on the frontend

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
+const cors = require('cors');
 const app = express();
+
+// Cors
+app.use(cors());
 
 app.use(
   '/graphql',
